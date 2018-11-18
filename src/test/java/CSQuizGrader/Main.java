4f7269ad192d1ce73fadb6368148f7b6cc31fb10
Roshan Prabhakar <roshan@aprabhakar.com>
2018-11-18 03:25:35 -0800

    Finished Collections class for complete Analysis; still need run jvm for runtime errors

--- src/test/java/CSQuizGrader/Main.java
+++ src/test/java/CSQuizGrader/Main.java
@@ -5,23 +5,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         try {
-            JavaFile jf = new JavaFile("src/TextSources/HelloWorld.txt", "HelloWorld.txt");
-            Compiler compiler = new Compiler("HelloWorld");
-
-            System.out.println("Compile-time Errors: ");
-            compiler.compile();
-
-            jf.updateJavaFile(jf.fixClassSyntax());
-            System.out.println("ERROR_LOG: ");
-            System.out.println(jf.getERROR_LOG());
-
-            System.out.println("Fixing errors...");
-            compiler.compile();
-
-            System.out.println("Running synthetic JVM...");
-            SyntheticJVM syntheticJVM = new SyntheticJVM("HelloWorld");
-            syntheticJVM.run();
-
+            Collections collections = new Collections();
+            collections.analyzeCollections();
         } catch (Exception e) {
             e.printStackTrace();
         }
