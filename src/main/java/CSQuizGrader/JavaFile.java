b59f6ed417c4657e4eccc3eb0b8e91d76dcd4d2a
young <youngyu2002@gmail.com>
2018-11-18 00:01:01 -0800

    Merge branch 'master' of https://github.com/roshanprabhakar/CSQuizGrader into young
    # Conflicts:
    #	src/TextSources/HelloWorld.txt
    #	src/main/java/CSQuizGrader/JavaFile.java

--- src/main/java/CSQuizGrader/JavaFile.java
+++ src/main/java/CSQuizGrader/JavaFile.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class JavaFile {
     public ArrayList<String> javaFile = new ArrayList<>();
-    private ArrayList<String> errorLog = new ArrayList<>();
+    private ArrayList<String> ERROR_LOG = new ArrayList<>();
     String fileName;
 
     public JavaFile(String filepath, String fileName) {
@@ -40,17 +40,22 @@ public class JavaFile {
         ArrayList<String> fixedCode = new ArrayList<>();
         ArrayList<String> words = new ArrayList<>();
 
+        int changeInBraces;
+        int changeInSemicolons;
+        int changeInParentheses;
+        int changeInSquareBrackets;
+
         for (int i = 0; i < javaFile.size(); i++) {
+            changeInBraces = getCountOfChar(javaFile.get(i), "{");
+            changeInSemicolons = getCountOfChar(javaFile.get(i), ";");
+            changeInParentheses = getCountOfChar(javaFile.get(i), ")");
+            changeInSquareBrackets = getCountOfChar(javaFile.get(i), "]");
+
+
             javaFile.set(i, updateClosedParenthesis(javaFile.get(i)));
-        }
+            javaFile.set(i, updateSquareBrackets(javaFile.get(i)));
 
-        for (int i = 0; i < javaFile.size(); i++) {
-
-            System.out.println(i);
             int indexOfSpace = 0;
-
-            int numOpenParenthesis = countOpenParenthesis(javaFile.get(i));
-            int numClosedParenthesis = countClosedParenthesis(javaFile.get(i));
 
             for (int j = 0; j < javaFile.get(i).length(); j++) { //split each among the spaces
                 if (javaFile.get(i).substring(j, j + 1).equals(" ")) {
@@ -59,21 +64,28 @@ public class JavaFile {
                 }
             }
             words.add(javaFile.get(i).substring(indexOfSpace));
-            System.out.println(words);
 
             //words has the individual words from each line of code
             if (words.get(0).equals("public") || words.get(0).equals("private") || words.get(0).equals("protected") ||
                     words.get(0).equals("for") || words.get(0).equals("while") || words.get(0).equals("if")) {
                 if (!words.get(words.size() - 1).equals("{")) {
                     words.add("{");
-
-                    errorLog.add("missing open brace at line: " + (i + 1));
                 }
             } else if (words.get(words.size() - 1).charAt(words.get(words.size() - 1).length() - 1) != ';') {
                 words.add(";");
-                errorLog.add("missing semicolon at line: " + (i + 1));
             }
             //add appropriate "}" to end of file
+
+            changeInBraces = getCountOfChar(concatenateList(words), "{") - changeInBraces;
+            changeInSemicolons = getCountOfChar(concatenateList(words), ";") - changeInSemicolons;
+            changeInParentheses = getCountOfChar(concatenateList(words), ")") - changeInParentheses;
+            changeInSquareBrackets = getCountOfChar(concatenateList(words), "]") - changeInSquareBrackets;
+
+            addToErrorLog(changeInBraces, "{", i);
+            addToErrorLog(changeInSemicolons, ";", i);
+            addToErrorLog(changeInParentheses, ")", i);
+            addToErrorLog(changeInSquareBrackets, "]", i);
+
             fixedCode.add(concatenateList(words));
             words.clear();
         }
@@ -82,24 +94,13 @@ public class JavaFile {
         return fixedCode;
     }
 
-    private int countOpenParenthesis(String str) {
-        int count = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '{') {
-                count++;
-            }
+    public void addToErrorLog(int change, String character, int lineNum) {
+        for (int j = 0; j < change; j++) {
+            ERROR_LOG.add("missing \"" + character + "\" at line: " + (lineNum + 1));
         }
-        return count;
-    }
-
-    private int countClosedParenthesis(String str) {
-        int count = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '}') {
-                count++;
-            }
+        for(int j = 0; j > change; j--){
+            ERROR_LOG.add("extra \"" + character + "\" at line: " + (lineNum + 1));
         }
-        return count;
     }
 
     private void addClosedBraces(ArrayList<String> fixedCode) {
@@ -127,11 +128,20 @@ public class JavaFile {
         return true;
     }
 
-    private String updateClosedParenthesis(String line) {
+    private int getCountOfChar(String line, String character) {
+        int count = 0;
+        for (int i = 0; i < line.length(); i++) {
+            if (line.substring(i, i + 1).equals(character)) {
+                count++;
+            }
+        }
+        return count;
+    }
 
+    private String updateClosedParenthesis(String line) {
         String lineCopy = line;
-        line = removeCharacter(line, ';');
-        line = removeCharacter(line, '{');
+        line = removeCharacter(line, "{");
+        line = removeCharacter(line, ";");
 
         line = line.trim();
 
@@ -166,6 +176,44 @@ public class JavaFile {
         return line;
     }
 
+    private String updateSquareBrackets(String line) {
+        String lineCopy = line;
+        line = removeCharacter(line, "{");
+        line = removeCharacter(line, ";");
+
+        line = line.trim();
+
+        if (hasOnlySpaces(line)) {
+            return lineCopy;
+        }
+
+        int openParenthesisCount = 0;
+        int closedParenthesisCount = 0;
+
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == '[') {
+                openParenthesisCount++;
+            }
+        }
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == ']') {
+                closedParenthesisCount++;
+            }
+        }
+
+        if (openParenthesisCount > closedParenthesisCount) {
+            for (int i = 0; i < openParenthesisCount - closedParenthesisCount; i++) {
+                line += ']';
+            }
+
+        } else {
+            for (int i = 0; i < closedParenthesisCount - openParenthesisCount; i++) {
+                line = removeOneInstanceOfCharacterFromEnd(line, ']');
+            }
+        }
+        return line;
+    }
+
     private String removeOneInstanceOfCharacterFromEnd(String line, Character character) {
         String output = "";
         int count = 0;
@@ -188,10 +236,10 @@ public class JavaFile {
         return newString;
     }
 
-    private String removeCharacter(String line, char character) {
+    private String removeCharacter(String line, String character) {
         String output = "";
         for (int i = 0; i < line.length(); i++) {
-            if (line.charAt(i) != character) {
+            if (line.charAt(i) != character.charAt(0)) {
                 output += line.charAt(i);
             }
         }
@@ -210,8 +258,8 @@ public class JavaFile {
         return output;
     }
 
-    public ArrayList<String> getErrorLog() {
+    public ArrayList<String> getERROR_LOG() {
         //need to get rid of repeated
-        return this.errorLog;
+        return this.ERROR_LOG;
     }
 }
--- src/test/java/CSQuizGrader/Main.java
+++ src/test/java/CSQuizGrader/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         ArrayList<String> correctedArguments = jf.fixClassSyntax();
 
-        System.out.println(jf.getErrorLog());
+        System.out.println(jf.getERROR_LOG());
         System.out.println(correctedArguments);
         jf.updateJavaFile(correctedArguments);
 
