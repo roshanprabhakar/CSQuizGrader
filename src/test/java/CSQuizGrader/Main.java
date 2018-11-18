7cebf3575eb917825c970e04683ad30b0f5532f3
young <youngyu2002@gmail.com>
2018-11-18 02:13:34 -0800

    fixed error log

--- src/test/java/CSQuizGrader/Main.java
+++ src/test/java/CSQuizGrader/Main.java
@@ -6,15 +6,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         try {
             JavaFile jf = new JavaFile("src/TextSources/HelloWorld.txt", "HelloWorld.txt");
-    //      ArrayList<String> correctedArguments = jf.fixClassSyntax();
-    //      jf.updateJavaFile(correctedArguments);
+            //      ArrayList<String> correctedArguments = jf.fixClassSyntax();
+            //      jf.updateJavaFile(correctedArguments);
 
             Compiler compiler = new Compiler("HelloWorld");
             compiler.compile();
 
             jf.updateJavaFile(jf.fixClassSyntax());
-            System.out.println("");
-
+            System.out.println();
+            System.out.println(jf.getERROR_LOG());
             compiler.compile();
 
         } catch (Exception e) {
