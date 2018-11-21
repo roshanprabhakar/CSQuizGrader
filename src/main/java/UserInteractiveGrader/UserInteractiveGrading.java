e8fc5253ff2c6d3dc671290fc5a6428ffaa43a4a
Roshan Prabhakar <roshan@aprabhakar.com>
2018-11-20 23:22:04 -0800

    removed faulty image code

--- src/main/java/UserInteractiveGrader/UserInteractiveGrading.java
+++ src/main/java/UserInteractiveGrader/UserInteractiveGrading.java
@@ -1,12 +1,9 @@
 package UserInteractiveGrader;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 
 import CSQuizGrader.*;
 
@@ -15,21 +12,16 @@ public class UserInteractiveGrading {
     private final String ALPHABET = "abcdefghijklmnopqrstuvwxyz"; //to auto-generate file names
     private final String separator = File.separator;
 
-    public void displayImageFromFile(String filename, String title) {
-        String path = "src" + separator + "ScannedImageSources" + separator + filename;
-
-        JFrame jFrame = new JFrame(path);
-
-
+    public void displayImageFromFile(String filename) {
+        //complete this
     }
-
 
     public void printLocalizedPartOfScreen(int topLeftX, int topLeftY, int height, int width) {
         try {
             //define image from screen
             Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
             Robot robot = new Robot();
-            BufferedImage img = robot.createScreenCapture( new Rectangle(topLeftX, topLeftY, width, height) );
+            BufferedImage img = robot.createScreenCapture(new Rectangle(topLeftX, topLeftY, width, height));
 
             //print it out
             JFrame frame = new JFrame();
