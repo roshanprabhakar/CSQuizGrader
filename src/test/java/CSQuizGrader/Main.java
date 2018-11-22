31eb8746236b745eefa3e446649f483e07b77fcc
Roshan Prabhakar <roshan@aprabhakar.com>
2018-11-21 20:49:35 -0800

    added detailed algorithm explanation

--- src/test/java/CSQuizGrader/Main.java
+++ src/test/java/CSQuizGrader/Main.java
@@ -1,25 +1,25 @@
 package CSQuizGrader;
 
-import UserInteractiveGrader.UserInteractiveGrading;
-
 public class Main {
     public static void main(String[] args) {
 //        runCollectionsAnalysis();
         userInteractiveGradingSystem();
     }
 
+    /**
+     * Algorithm:
+     * 1. display blank test
+     * 2. User goes through test and drags boxes on the parts where there will be answers
+     * 3. for every question:
+     *      loop through all the responses, display them side by side
+     *      each should have two input boxes under them: 1 for displaying sentence comments,
+     *      the other for typing a relative score for that problem
+     * 4. record all data to associated students
+     * 5. generate a class summary with all comments and general scores (which questions did people get wrong the most
+     * 6. generate student summary, with comments and scores
+     * 7. send these scores through email
+     */
     public static void userInteractiveGradingSystem() {
-
-        UserInteractiveGrading userInteractiveGrading = new UserInteractiveGrading();
-        userInteractiveGrading.displayImageFromFile("testingImg1.jpg");
-
-
-    //        try {
-    //            UserInteractiveGrading userInteractiveGrading = new UserInteractiveGrading();
-    //            userInteractiveGrading.printLocalizedPartOfScreen(100,100,500,500);
-    //        } catch (Exception e) {
-    //            e.printStackTrace();
-    //        }
 
     }
 
